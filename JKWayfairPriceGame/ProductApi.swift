2a7698c461a1d2502972f6dfec9387880ccb9f3c
Jayesh Kawli <j.kawli@gmail.com>
2016-08-20 23:56:31 -0400

    WIP refactoring Product API to pass Mantle objects back instead of NSManagedObject

--- JKWayfairPriceGame/ProductApi.swift
+++ JKWayfairPriceGame/ProductApi.swift
@@ -18,6 +18,7 @@ enum DataFormat: String {
 
 enum ModelType: String {
     case Product
+    case ProductCategory
 }
 
 enum ProductStorageIndicatorKey: String {
@@ -40,7 +41,10 @@ class ProductApi {
         Alamofire.request(.GET, self.baseURLString, parameters: ["category_id": categoryIdentifier, "_format": format.rawValue], encoding: .URL, headers: nil).validate(statusCode: 200..<300).responseJSON { response in
             switch response.result {
             case .Success(let data):
-                if let data = data as? [String: AnyObject], collection = data["product_collection"] as? [[String: AnyObject]] {
+                if let data = data as? [String: AnyObject] {
+                    
+                    if let collection = data["product_collection"] as? [[String: AnyObject]] {
+                    
                         do {
                             if let productsCollection = try MTLJSONAdapter.modelsOfClass(Product.self, fromJSONArray: collection) as? [Product] {
                                 
@@ -54,6 +58,15 @@ class ProductApi {
                         } catch let error as NSError {
                             completion(.Failure(error))
                         }
+                    } else if let collection = data["subcategories"] as? [[String: AnyObject]] {
+                        do {
+                            if let productCategoriesCollection = try MTLJSONAdapter.modelsOfClass(ProductCategory.self, fromJSONArray: collection) as? [ProductCategory] {
+                                
+                            }
+                        } catch let error as NSError {
+                            completion(.Failure(error))
+                        }
+                    }
                 } else {
                     completion(.Failure(NSError(domain: "WFProducts", code: 100, userInfo: [NSLocalizedDescriptionKey: "Could not parse incoming json to expected format"])))
                 }
